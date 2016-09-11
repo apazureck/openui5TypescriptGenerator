81f170bc3559dd771fbdff4aafa119f3d0acf37c
Andreas Pazureck <pazureck@c-s-g.net>
2016-09-11 13:06:03 +0200

    - Type gets now generated simply with a namespace tag around - First steps of doctypes conversion to type conversion. - Creates overloads, if leading parameters are optional   (currently creating only one extra overload with the leading optional parameters as mandatory parameters)

--- UI5TypescriptGenerator/UI5TypescriptGenerator/Program.cs
+++ UI5TypescriptGenerator/UI5TypescriptGenerator/Program.cs
@@ -20,6 +20,13 @@ namespace UI5TypescriptGenerator
 
             ClassDefinition tst = new ClassDefinition(doc);
 
+            StringBuilder nscontent = new StringBuilder();
+            nscontent.AppendLine("namespace " + tst.Namespace + " {");
+            tst.CreateDefinitionString(nscontent, 1);
+            nscontent.AppendLine("}");
+            Console.Write(nscontent.ToString());
+            Console.Read();
+
             //var node = doc.DocumentNode.Descendants("h1").First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("classTitle"));
             //classdef += node.Descendants("span").First(x => x.Attributes["class"] != null && x.Attributes["class"].Value.Contains("package")).InnerText.TrimEnd(".".ToCharArray());
         }
@@ -75,8 +82,14 @@ namespace UI5TypescriptGenerator
                 var sectionItems = detailsection.Descendants("div").Where(x => x.Attributes["class"] != null ? x.Attributes["class"].Value.Contains("sectionItem") : false);
                 foreach (var sectionItem in sectionItems)
                 {
-                    MethodDescription md = CreateMethodDescription(sectionItem);
-                    mdlist.Add(md);
+                    try
+                    {
+                        MethodDescription md = CreateMethodDescription(sectionItem);
+                        mdlist.Add(md);
+                    }
+                    catch (Exception)
+                    {
+                    }
                 }
             }
 
@@ -99,6 +112,7 @@ namespace UI5TypescriptGenerator
                 foreach (var sectionItem in sectionItems)
                 {
                     ConstructorDescription ctor = new ConstructorDescription(CreateMethodDescription(sectionItem));
+                    ctor.Name = "constructor";
                     try
                     {
                         var parameterlist = sectionItem.Descendants("ul").First().Descendants("li").First(x => x.Descendants("#text").FirstOrDefault(y => y.InnerText.Contains("Properties")) != null).Descendants("ul").FirstOrDefault().Descendants("li");
@@ -139,7 +153,7 @@ namespace UI5TypescriptGenerator
             }
             md.Attributes = sectionItem.Attributes["class"].Value.Split().ToList();
             var description = sectionItem.Descendants("div").FirstOrDefault(x => x.Attributes["class"] != null ? x.Attributes["class"].Value.Contains("full-description") : false);
-            md.Description = description.InnerText.Trim();
+            md.Description = description.InnerHtml.Trim();
             var paramtables = sectionItem.Descendants("table").Where(x => x.Attributes["class"] != null ? x.Attributes["class"].Value.Contains("methodItem") : false)
                 .Select(x => new { category = x.ParentNode.ChildNodes[x.ParentNode.ChildNodes.IndexOf(x) - 2].InnerText.TrimEnd(":".ToCharArray()), table = x });
 
@@ -192,6 +206,96 @@ namespace UI5TypescriptGenerator
             if (descriptionnode != null)
                 tst.Description = descriptionnode.InnerText;
         }
+        
+        #region Typescript class generation
+        public void CreateDefinitionString(StringBuilder namespacecontent, int tablevel)
+        {
+            StringBuilder references = new StringBuilder();
+            StringBuilder classcontent = new StringBuilder();
+
+            CreateClassContent(classcontent);
+
+            CreateInterfaceDescriptions(classcontent, tablevel);
+            namespacecontent.AppendLine("", tablevel);
+            namespacecontent.AppendLine("class " + Name + (string.IsNullOrWhiteSpace(Baseclass) ? "" : " extends " + Baseclass) + " {", tablevel);
+            namespacecontent.AppendLine(classcontent.ToString(), tablevel+1);
+            namespacecontent.AppendLine("}", tablevel);
+
+        }
+
+        private void CreateInterfaceDescriptions(StringBuilder classcontent, int tablevel)
+        {
+            // TODO Impelemnt
+        }
+
+        private void CreateClassContent(StringBuilder classcontent)
+        {
+            // Constructor
+            foreach (ConstructorDescription cd in Constructors)
+            {
+                classcontent.AppendLine();
+                AppendMethod(classcontent, cd);
+            }
+            foreach (MethodDescription md in Methods)
+            {
+                classcontent.AppendLine();
+                AppendMethod(classcontent, md);
+            }
+        }
+
+        private static void AppendMethod(StringBuilder classcontent, MethodDescription cd)
+        {
+            foreach(string s in cd.CreateMethodStubs())
+            {
+                classcontent.AppendLine();
+                classcontent.AppendLine(s + ";");
+            }
+        } 
+        #endregion
+    }
+
+    public static class StringBuilderExtensions
+    {
+        /// <summary>
+        /// Append string as comment (fill in string without any comment prefixes.
+        /// </summary>
+        /// Result will be
+        /// <example>
+        /// <code>
+        ///  /** <br/>
+        ///  * Your Comment lines <br/>
+        ///  */
+        /// </code>
+        /// </example>
+        /// <param name="sb"></param>
+        /// <param name="commenttext"></param>
+        public static void AppendComment(this StringBuilder sb, string commenttext)
+        {
+            sb.AppendLine("/**");
+            using (StringReader sr = new StringReader(commenttext))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                    sb.AppendLine(" * " + line);
+            }
+            sb.AppendLine(" */");
+        }
+
+        /// <summary>
+        /// Appens a new line with beginning tabs.
+        /// </summary>
+        /// <param name="sb">extension</param>
+        /// <param name="value">string to append</param>
+        /// <param name="tabs">tabs to put in front</param>
+        public static void AppendLine(this StringBuilder sb, string value, int tabs)
+        {
+            using (StringReader sr = new StringReader(value))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                    sb.AppendLine(new String('\t', tabs) + line);
+            }
+        }
     }
 
     public class InterfaceDefinition
@@ -203,8 +307,6 @@ namespace UI5TypescriptGenerator
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class ConstructorDescription : MethodDescription
     {
-        private MethodDescription methodDescription;
-
         public ConstructorDescription(MethodDescription methodDescription)
         {
             Attributes = methodDescription.Attributes;
@@ -214,36 +316,153 @@ namespace UI5TypescriptGenerator
             ReturnType = methodDescription.ReturnType;
         }
 
-        private string DebuggerDisplay => $"Method: " + ToString();
-        public override string ToString()
-        {
-            return $"{ ReturnType} {Name}(" + Parameters.Aggregate("", (a, b) => a + ", " + b.Name + ":" + b.Type).TrimStart(", ".ToCharArray()) + ")";
-        }
+        private string DebuggerDisplay => $"Constructor: " + ToString();
     }
 
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class MethodDescription
     {
-        public string Name { get; set; } = "";
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if(value!=null && Regex.IsMatch(value, @"\W"))
+                    throw new ArgumentException("Not a valid Type name");
+                name = value;
+            }
+        }
+
         public List<Parameter> Parameters { get; set; } = new List<Parameter>();
-        public string ReturnType { get; set; } = "";
+        private string returnType = "";
+
+        public string ReturnType
+        {
+            get { return returnType; }
+            set { returnType = GlobalTypeManager.ConvertToValidTypeIfKnown(value); }
+        }
+
         public string Description { get; set; } = "";
 
         public List<string> Attributes { get; set; } = new List<string>();
+
         private string DebuggerDisplay => $"Method: " + ToString();
-        public override string ToString()
+
+        public virtual string[] CreateMethodStubs()
         {
-            return $"{ ReturnType} {Name}(" + Parameters.Aggregate("", (a, b) => a + ", " + b.Name + ":" + b.Type).TrimStart(", ".ToCharArray()) + ")";
+            int lastnotoptionalindex = Parameters.IndexOf(Parameters.LastOrDefault(x => !x.IsOptional));
+            // Last optional parameter is first parameter -> ok
+            if (lastnotoptionalindex <= 0)
+                return new string[] { CreateDefaultStub() };
+
+            // Otherwise you have to check if there are optional parameters before
+            int firstoptionalindex = Parameters.IndexOf(Parameters.FirstOrDefault(x => x.IsOptional));
+
+            // Only non optional indizes are before the first optional
+            if (firstoptionalindex > -1 && firstoptionalindex < lastnotoptionalindex)
+            {
+                List<string> retlist = new List<string>();
+                // Create overload without leading optional parameters
+                List<Parameter> plit = Parameters.Select(p => p.Clone() as Parameter).ToList();
+                retlist.Add(CreateStubWithParameters(plit.SkipWhile(x=> x.IsOptional)));
+                // Create Overload with optional leading parameters as not optional parameters
+                plit.TakeWhile(x => x.IsOptional).ToList().ForEach(x => x.Name = x.Name.Replace("?", ""));
+                retlist.Add(CreateStubWithParameters(plit));
+                return retlist.ToArray();
+            }
+
+            // Have to create overload.
+
+            return new string[] { CreateDefaultStub() };
+        }
+
+        private string CreateStubWithParameters(IEnumerable<Parameter> parameters)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendComment(CreateTypescriptMethodDescription(parameters));
+            sb.AppendLine($"{Modifier} {Name}(" + parameters.Aggregate("", (a, b) => a + ", " + b.Name + ":" + b.Type).TrimStart(", ".ToCharArray()) + ")" + (string.IsNullOrWhiteSpace(ReturnType) ? "" : " : " + ReturnType));
+            return sb.ToString();
+        }
+
+        private string CreateDefaultStub()
+        {
+            return CreateStubWithParameters(Parameters);
+        }
+
+        private string CreateTypescriptMethodDescription(IEnumerable<Parameter> parameters)
+        {
+            // Append Description:
+            StringBuilder description = new StringBuilder();
+            description.AppendLine(Description);
+            foreach (Parameter p in parameters)
+                description.AppendLine("@param " + p.Name + " " + p.Description);
+
+            string s = description.ToString();
+            return s;
+        }
+
+        public string Modifier
+        {
+            get
+            {
+                if (Attributes.Contains("protected"))
+                    return "protected";
+                return "";
+            }
+        }
+
+        public override string ToString() => $"{Modifier} {Name}(" + Parameters.Aggregate("", (a, b) => a + ", " + b.Name + ":" + b.Type).TrimStart(", ".ToCharArray()) + ")" + (string.IsNullOrWhiteSpace(ReturnType) ? "" : " : " + ReturnType);
+    }
+
+    /// <summary>
+    /// Manages all that has to do with types
+    /// </summary>
+    public static class GlobalTypeManager
+    {
+        private static Dictionary<string, string> TranslationDictionary = Properties.Settings.Default.TypeTranslations.Cast<string>().ToDictionary(key => key.Split()[0], val => val.Split()[1]);
+        public static string ConvertToValidTypeIfKnown(string inval)
+        {
+            string[] values = inval.Split('|');
+            List<string> retvalues = new List<string>(values.Length);
+            foreach (string value in values)
+                if (TranslationDictionary.ContainsKey(value))
+                    retvalues.Add(TranslationDictionary[value]);
+                else
+                    retvalues.Add(value);
+            return retvalues.Aggregate((a, b) => a + "|" + b);
         }
     }
 
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
-    public class Parameter
+    public class Parameter : ICloneable
     {
-        public string Type { get; set; }
+        private string type;
+
+        public string Type
+        {
+            get { return type; }
+            set
+            {
+                type = GlobalTypeManager.ConvertToValidTypeIfKnown(value);
+            }
+        }
+
         public string Name { get; set; }
         public string Description { get; set; }
         private string DebuggerDisplay => $"Parameter '{Name}: {Type}'";
+        public bool IsOptional => Name?.Contains('?') == true;
+
+        public object Clone()
+        {
+            return new Parameter()
+            {
+                Description = Description,
+                Name = Name,
+                Type = Type
+            };
+        }
     }
 
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
